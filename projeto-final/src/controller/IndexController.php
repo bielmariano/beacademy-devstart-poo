ef146e07adb352626a5ea89a2b4ced7d4cc36ed4
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-06-06 16:51:52 -0300

    25 - 26 controller

---
+++ projeto-final/src/controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+class IndexController {
+    public function indexAction(): void
+    {
+        echo "Ola mundo, estou dentro de uma action, dentro do controller";
+    }
+}

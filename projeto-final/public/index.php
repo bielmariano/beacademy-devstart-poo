0735ca9a83a2a6931d91bc2bedc0bc2e50420ca9
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-06-06 21:29:09 -0300

    aula 27 | controller - view

--- projeto-final/public/index.php
+++ projeto-final/public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Ola mundo';
+ini_set('display_errors', 1);
+
+include '../vendor/autoload.php';
+
+use App\Controller\IndexController;
+
+$c = new IndexController();
+// $c->indexAction();
+$c->loginAction();
+
+// echo 'Ola mundo';
--- projeto-final/src/controller/IndexController.php
+++ projeto-final/src/controller/IndexController.php
@@ -2,9 +2,17 @@
 
 declare(strict_types=1);
 
+namespace App\Controller;
+
 class IndexController {
     public function indexAction(): void
     {
-        echo "Ola mundo, estou dentro de uma action, dentro do controller";
+        //__DIR__  sinaliza a pasta onde está, e a cada dirname voltará uma pasta.
+        include dirname(__DIR__).'/View/index/index.php';
+    }
+
+    public function loginAction(): void
+    {
+        include dirname(__DIR__).'/View/index/login.php';
     }
 }
